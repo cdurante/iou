9f6823e5e63466e01097dfe693d1e0e17b62a13e
Caleb <durante@wisc.edu>
2014-04-01 22:04:57 -0500

    Started a new Rails project from scratch, Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iou::Application.config.secret_key_base = '7eb548b4e30df7eee55861ea8880ac05c4f352a702b96ac7fb71316fabd2378874b37b7982bd7f0832f68ce69d601cd8a317af52dbd7b1cf1197bf2f84aaa39b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Iou::Application.config.session_store :cookie_store, key: '_iou_session'
