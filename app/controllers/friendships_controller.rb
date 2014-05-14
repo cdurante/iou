b24e7fa3aee31c61a0dc9af54acccd8197bfcaa3
Caleb <durante@wisc.edu>
2014-05-13 20:49:38 -0500

    Can delete friendships

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,26 @@
 class FriendshipsController < ApplicationController
+  before_action :set_friendship, only: [:show, :edit, :update, :destroy]
 
-  def create
+  # GET /friendships
+  def index
+    @friendships = Friendship.all
+  end
+
+  # GET /friendships/1
+  def show
+  end
+
+  # GET /friendships/new
+  def new
+    @friendship = Friendship.new
+  end
+
+  # GET /friendships/1/edit
+  def edit
+  end
+
+  # POST /friendships
+   def create
     if current_user.id != params[:friend_id]
       @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
       if @friendship.save
@@ -13,10 +33,32 @@ class FriendshipsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /friendships/1
+  def update
+    if @friendship.update(friendship_params)
+      redirect_to @friendship, notice: 'Friendship was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  # DELETE /friendships/1
   def destroy
     @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
     flash[:notice] = "Removed friendship."
     redirect_to current_user
   end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_friendship
+      @friendship = Friendship.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def friendship_params
+      params[:friendship]
+    end
 end
