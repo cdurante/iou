3bdb5fc72824bee336981d7ccc4ca044e408a25e
Jake Hilborn <jhilborn@wisc.edu>
2014-05-13 22:22:02 -0500

    cleaned up rspec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,18 @@
 FactoryGirl.define do
  factory :user do
-   name	"Jake Hilborn"
-   email "name@domain.com"
-   password "passwd"
-   password_confirmation "passwd"
+   sequence(:name)  { |n| "Person #{n}" }
+   sequence(:email) { |n| "person_#{n}@example.com"}
+   password "foobar"
+   password_confirmation "foobar"
    admin "TRUE"
  end
+
+ #factory :user do
+ #  name	"User 2"
+ #  email "user2@domain.com"
+ #  password "passwd"
+ #  password_confirmation "passwd"
+ #end
 
  factory :transaction do
     name "Lorem ipsum"
--- spec/requests/transactions_spec.rb
+++ spec/requests/transactions_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe "Transactions" do
+  
   @user1=User.new(name: "User1Name", email: "user1@example.com", password: "foobar" , password_confirmation: "foobar")
   @Transaction1=Transaction.new(name: "Transaction1", amount: "5", debtor_id: "2", description: "This is a description for transactions")
+  
   describe "valid transactions" do
+  	
   end
 end
